04c8b3ac81963e8a4165fa28fe3d6fb3e0092baa
polthagorn <6610685262@cn330>
2026-04-21 22:14:07 +0700

    Add email sending test

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emails',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "6610685262@student.tu.ac.th"
+EMAIL_HOST_PASSWORD = "ifsu rmau amwm hoaz"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
