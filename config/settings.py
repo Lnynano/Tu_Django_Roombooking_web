3deaf69f7538f5f9463e6bd100f554dee4f52087
Kittichet Sintaosathapon <6610685106@cn351>
2026-04-28 21:23:32 +0700

    change to smtp

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 LOGIN_URL = "accounts:login"
 
 # Email Configuration for Development
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
